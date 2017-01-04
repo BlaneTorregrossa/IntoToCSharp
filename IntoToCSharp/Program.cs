9fb866b47ab4c71f32492fd6811d4baddeab99b3
BlaneTorregrossa <blanet97@cox.net>
2017-01-04 17:57:04 -0600

    Initial commit

---
+++ IntoToCSharp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroCSharp
+{
+    class Entity
+    {
+        public Entity() { }
+        //M.I.
+        private int health;
+        protected int Health;
+        private int attackPower;
+        protected int AttackPower;
+        //M.I.
+        private string name;
+        public string Name;
+        }
+    }
+
+
+class Zombie
+{
+    public Zombie() { }
+    //M.I.
+    //M.I.
+
+}
+
+class Player
+{
+    public Player() { }
+    //M.I.
+    //M.I.
+    //M.I.
+
+    class Student
+    {
+        public Student() { }
+        public Student;
+
+        private int id;
+
+
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List <Student> students;
+            // Create a new student called s and then type s. to see what appears
+            // String one of the Students
+        }
+    }
+
+}
